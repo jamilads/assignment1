3a5ff6309e699ad161f9e9725427852add0f8cdb
user142219 <user142219@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2018-09-11 20:48:32 -0400

    Initial Commit

---
+++ assignment1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  assignment1
+//
+//  Created by user142219 on 9/11/18.
+//  Copyright © 2018 user142219. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
